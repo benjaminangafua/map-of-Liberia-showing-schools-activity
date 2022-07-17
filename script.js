56a6e94d2d63c40a0ba152f7f9d927583702777d
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-07-17 23:18:24 +0000

    Added classes to counties to make callable

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ fetch(URL_PATH)
             "GrandBassa", "GrandCapeMount", "Nimba", "RiverGee", "RiveCess",
             "Gbarpolu", "Sinoe", "Maryland", "Margibi", "Montserrado"
         ]
+        let Lofa = document.querySelectorAll(`.${counties[0]}`)
+        Lofa[0].setAttribute("style", "fill:blue")
+        console.log()
 
         for (let i = 0; i < counties.length; i++) {
-            let elementEffect = document.getElementById(counties[i])
-            console.log(elementEffect)
+            // let elementEffect = document.getElementById(counties[i])
+            // console.log(elementEffect)
         }
         console.log(data)
 
