4e7f0893ee0cb3d684b6c03515991533f3edbc6a
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-07-17 22:52:28 +0000

    Updated the counties as id

---
+++ script.js
@@ -0,0 +1,19 @@
+const URL_PATH = "./schools.json"
+
+fetch(URL_PATH)
+    .then(res => res.json())
+    .then(data => {
+
+        let counties = ["Lofa", "Bomi", "Bong", "GrandGedeh", "GrandKru",
+            "GrandBassa", "GrandCapeMount", "Nimba", "RiverGee", "RiveCess",
+            "Gbarpolu", "Sinoe", "Maryland", "Margibi", "Montserrado"
+        ]
+
+        for (let i = 0; i < counties.length; i++) {
+            let elementEffect = document.getElementById(counties[i])
+            console.log(elementEffect)
+        }
+        console.log(data)
+
+
+    }).catch(error => console.error({ error }))
