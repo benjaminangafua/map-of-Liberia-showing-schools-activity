c0299868a1825efcb830a1e69fb48a6e4a7ba20e
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-07-21 02:20:17 +0000

    Add pop up and other update

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 const URL_PATH = "./schools.json"
+let container = document.querySelector(".container")
 
 fetch(URL_PATH)
     .then(res => res.json())
     .then(data => {
         let COUNTIES = []
-        let container = document.querySelector(".container")
 
         // Get counties
-        let removeDuplicate = data.filter((obj, pos, arr) => arr.map(mapObj => mapObj["County Name"]).indexOf(obj["County Name"]) == pos);
+        let removeDuplicate = data.filter((obj, pos, arr) => arr.map(mapObj =>
+            mapObj["County Name"]).indexOf(obj["County Name"]) == pos);
         removeDuplicate.forEach(element => COUNTIES.push(element["County Name"]));
 
 
-        const COLOR = ["green", "orange", "navy", "purple", "brown", "red", "teal", "black", "yellow", "khaki", "gray", "pink", "blue", "lime", "crimson"]
+        const COLOR = [
+            "green", "orange", "navy", "purple", "brown", "red", "teal",
+            "black", "yellow", "khaki", "gray", "pink", "blue", "lime", "crimson"
+        ]
 
         for (let i = 0; i < COUNTIES.length; i++) {
-            console.log(i)
-            let count = document.querySelectorAll(`.${COUNTIES[i]}`)
-            for (let j = 0; j < count.length; j++) {
-                count[j].setAttribute("style", `fill:${COLOR[i]}`)
 
+            let eachCounty = document.querySelectorAll(`.${COUNTIES[i]}`)
+
+
+            for (let j = 0; j < eachCounty.length; j++) {
+
+                // Color counties
+                eachCounty[j].setAttribute("style", `fill:${COLOR[i]}`)
+
+                // Hover
+                eachCounty[j].addEventListener("mouseover", (event) => {
+                    console.log(COUNTIES[i])
+                })
+
+                // Pop up
+                // let span = document.createElement("span")
+                // span.setAttribute("class", `popUp`)
+                // container.append(span)
             }
         }
 
         let animate = document.querySelectorAll(".container span")
         for (let i = 0; i < animate.length; i++) {
             animate[i].style = `
-            position: absolute;
-            width: 15px;
-            height: 15px;
-            border-radius: 50px;
-            background-color: #fff;
+            position: absolute; width: 15px; height: 15px;
+            border-radius: 50px; background-color: #fff;
            animation: popUp 1.5s linear infinite;
         `
         }
